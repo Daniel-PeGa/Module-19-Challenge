9269d5fdf2a117731f97518ed7d640e4d3ff5bc4
Daniel Perez Garnica <danielperezgarnic@gmail.com>
2022-10-25 08:09:05 -0500

    some database progress on client side

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,23 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  const jateDB = await openDB("jate", 1);
+  const tx = jateDB.trsansaction("jate", "readwrite");
+  const store = tx.ObjectStore("jate");
+  const request = store.put({ jate: content });
+  cpnst result = await request;
+  console.log('saved', result);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const jateDB = await openDB("jate", 1);
+  const tx = jateDB.transaction("jate", "readonly");
+  const store = tx.ObjectStore("jate");
+  const request = store.getAll();
+  const result = await request;
+  console.log('showing', result);
+};
 
 initdb();
